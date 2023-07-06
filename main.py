2bcab9a7fa42d5440a500025e7f5db74e4e99080
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-07-06 16:36:13 +0900

    configを出力

--- main.py
+++ main.py
@@ -536,6 +536,7 @@ if __name__ == "__main__":
         lightning_config.trainer = trainer_config
 
         # model
+        print(config)
         model = instantiate_from_config(config.model)
 
         # trainer and callbacks
